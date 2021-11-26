2c9d4ecc5efd1c41351ad491e8052f687e5258aa
Michael Charara <mic8612@g.harvard.edu>
2021-11-26 00:29:51 -0500

    Data science begin & miniLinter

---
+++ Full-Stack Engineer/02_Javascript_Syntax_Part2/miniLinter.js
@@ -0,0 +1,51 @@
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+const storyWords = story.split(' ');
+// console.log(storyWords.length);
+
+// const betterWords = storyWords.filter(word => word != unnecessaryWords[0] && word != unnecessaryWords[1] && word != unnecessaryWords[2]);
+const betterWords = storyWords.filter(word => !unnecessaryWords.includes(word));
+// console.log(betterWords.length);
+
+let reallyCount = 0;
+let veryCount = 0;
+let basicallyCount = 0;
+
+for(word of storyWords) {
+  if (word === overusedWords[0]) {
+    reallyCount++;
+  } else if (word === overusedWords[1]) {
+    veryCount++;
+  } else if (word === overusedWords[2]){
+    basicallyCount++;
+  } 
+};
+
+// console.log(`really Count: ${reallyCount} \nveryCount: ${veryCount} \nbasicallyCount: ${basicallyCount}`);
+
+let sentenceCount = 0
+
+for(word of storyWords) {
+  let end = word.length - 1;
+  if(word[end] === '!'){
+    sentenceCount++;
+  } else if (word[end] === '.'){
+    sentenceCount++
+  }
+};
+
+// console.log(sentenceCount);
+
+console.log(`
+Total Words: ${storyWords.length}
+Better Words: ${betterWords.length}
+really Count: ${reallyCount} 
+veryCount: ${veryCount} 
+basicallyCount: ${basicallyCount}`
+);
+
+console.log(betterWords.join(' '));
